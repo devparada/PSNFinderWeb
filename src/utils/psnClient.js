045461a59146ec5805d58e500fa7df8f8cc3a6a9
Raúl <raul.parada040103@gmail.com>
2024-11-08 15:38:05 +0100

    Pruebas para obtener datos de PSN + añado las dependencias

---
+++ src/utils/psnClient.js
@@ -0,0 +1,34 @@
+import { exchangeCodeForAccessToken, exchangeNpssoForCode, makeUniversalSearch, getUserTitles } from "psn-api";
+import dotenv from "dotenv";
+
+dotenv.config();
+var TOKEN_PSN_ENV;
+// El username es recibido como parámetro
+export const obtenerUsuarioData = async (username) => {
+  try {
+    if (process.env.NODE_ENV == "production") {
+      TOKEN_PSN_ENV = process.env.TOKEN_PSN_PROD;
+    } else {
+      TOKEN_PSN_ENV = process.env.TOKEN_PSN;
+    }
+
+    const TOKEN_PSN = TOKEN_PSN_ENV;
+    TOKEN_PSN_ENV = "";
+    // Autoriza para buscar el usuario
+    const accessCode = await exchangeNpssoForCode(TOKEN_PSN);
+    const authorization = await exchangeCodeForAccessToken(accessCode);
+
+    // Obtiene todos los datos del usuario
+    const allAccountsSearchResults = await makeUniversalSearch(authorization, username, 'SocialAllAccounts');
+    const targetAccountId = allAccountsSearchResults?.domainResponses[0]?.results[0]?.socialMetadata?.accountId;
+
+    // Obtiene los juegos del usuario
+    const { trophyTitles } = await getUserTitles(authorization, targetAccountId);
+
+    // Devuelve el nombre del usuario y los juegos
+    return { username, trophyTitles };
+  } catch (error) {
+    console.error('Error al obtener datos del usuario:', error);
+    return null;
+  }
+};

4fad745de8bd50ef7884f15de17a3fa1a50522dc
Raúl <raul.parada040103@gmail.com>
2024-11-08 16:43:01 +0100

    Varias pruebas en psnClient

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,11 @@ import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+    optimizeDeps: {
+      include: ['psn-api']
+    }
+  },
   output: 'server',
   adapter: vercel(),
   integrations: [tailwind()]
